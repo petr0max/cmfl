0eff02d83b1823a743e5b225bd7cfee5007fbef3
mardin <mardin@localhost.localdomain>
2021-09-21 07:11:47 +0700

    initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Consectetur est velit nam repellendus corporis Aspernatur est similique delectus debitis ab quidem Sint repellendus accusantium officia laboriosam ducimus. Ullam sunt blanditiis architecto labore error vitae? Voluptate beatae perferendis suscipit',
+            'author': 'Brad Traversy',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Consectetur est velit nam repellendus corporis Aspernatur est similique delectus debitis ab quidem Sint repellendus accusantium officia laboriosam ducimus. Ullam sunt blanditiis architecto labore error vitae? Voluptate beatae perferendis suscipit',
+            'author': 'Brad Traversy',
+            'create_date': '04-26-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Consectetur est velit nam repellendus corporis Aspernatur est similique delectus debitis ab quidem Sint repellendus accusantium officia laboriosam ducimus. Ullam sunt blanditiis architecto labore error vitae? Voluptate beatae perferendis suscipit',
+            'author': 'Brad Traversy',
+            'create_date': '04-27-2017'
+        },
+    ]
+    return articles
